293c1a7cd379867ddd27fbe714fb0c44920af0e5
Buivol16 <khmaradenis64@gmail.com>
2023-01-06 15:56:53 +0200

    Initial commit

--- src/main/java/ua/denis/bot/Bot.java
+++ src/main/java/ua/denis/bot/Bot.java
@@ -5,6 +5,7 @@ import com.pengrad.telegrambot.TelegramBot;
 import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.logging.log4j2.Log4J2LoggingSystem;
 import org.springframework.context.annotation.Bean;
@@ -15,17 +16,36 @@ import ua.denis.bot.listeners.MyListener;
 @NoArgsConstructor
 public class Bot {
 
+    @Autowired
+    MyListener myListener;
+
     Logger logger = LoggerFactory.getLogger(Bot.class);
 
-    TelegramBot telegramBot = null;
+    private static TelegramBot telegramBot = null;
 
     @Value("${bot.token}")
     private String token;
 
+
     @Bean
     private void createBot(){
         telegramBot = new TelegramBot(token);
-        telegramBot.setUpdatesListener(new MyListener());
-        logger.info("Bot is successfully run");
+        telegramBot.setUpdatesListener(myListener);
+        logger.info("                \n" +
+                "                    \n" +
+                "                //  \n" +
+                "             ///    \n" +
+                "           ///      \n" +
+                "  //     ///        \n" +
+                "  //// ///          \n" +
+                "   //////           \n" +
+                "    ///             \n" +
+                "________________________\n" +
+                "Telegram bot is successfully run" +
+                "\n");
+    }
+
+    public static TelegramBot getTelegramBot() {
+        return telegramBot;
     }
 }
--- src/main/java/ua/denis/bot/listeners/MyListener.java
+++ src/main/java/ua/denis/bot/listeners/MyListener.java
@@ -1,14 +1,35 @@
 package ua.denis.bot.listeners;
 
+import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ua.denis.bot.Bot;
+import ua.denis.bot.services.CatFactService;
 
 import java.util.List;
 
+@Component
+@NoArgsConstructor
 public class MyListener implements UpdatesListener {
+
+    @Autowired
+    CatFactService catFactService;
+
+    private TelegramBot bot = null;
+
     @Override
     public int process(List<Update> list) {
-        list.forEach(System.out::println);
+        bot = Bot.getTelegramBot();
+        list.forEach(update ->{
+            long chatId = update.message().chat().id();
+            Message message = update.message();
+            if (message != null && message.text().equals("/getfact")) bot.execute(new SendMessage(chatId, catFactService.getFact().toString()));
+        });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 }
